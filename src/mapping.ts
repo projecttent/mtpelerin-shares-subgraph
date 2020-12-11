c2702295efc24c251bb401a97827df2ce2c62479
projecttent <projecttent@users.noreply.github.com>
2020-12-10 23:16:57 -0800

    Add mappings

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,61 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import { Transfer } from '../generated/MPSToken/mps'
+import {
+  User,
+  UserCounter,
+  TransferCounter,
+  TotalSupply
+} from '../generated/schema'
+import { BigInt } from '@graphprotocol/graph-ts'
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+export function handleTransfer(event: Transfer): void {
+  let day = (event.block.timestamp / BigInt.fromI32(60 * 60 * 24))
+
+  let userFrom = User.load(event.params.from.toHex())
+  if (userFrom == null) {
+    userFrom = newUser(event.params.from.toHex(), event.params.from.toHex());
+  }
+  userFrom.balance = userFrom.balance - event.params.value
+  userFrom.transactionCount = userFrom.transactionCount + 1
+  userFrom.save()
+
+  let userTo = User.load(event.params.to.toHex())
+  if (userTo == null) {
+    userTo = newUser(event.params.to.toHex(), event.params.to.toHex());
+
+    // UserCounter
+    let userCounter = UserCounter.load('singleton')
+    if (userCounter == null) {
+      userCounter = new UserCounter('singleton')
+      userCounter.count = 1
+    } else {
+      userCounter.count = userCounter.count + 1
+    }
+    userCounter.save()
+    userCounter.id = day.toString()
+    userCounter.save()
+  }
+  userTo.balance = userTo.balance + event.params.value
+  userTo.transactionCount = userTo.transactionCount + 1
+  userTo.save()
+
+  // Transfer counter total and historical
+  let transferCounter = TransferCounter.load('singleton')
+  if (transferCounter == null) {
+    transferCounter = new TransferCounter('singleton')
+    transferCounter.count = 0
+    transferCounter.totalTransferred = BigInt.fromI32(0)
+  }
+  transferCounter.count = transferCounter.count + 1
+  transferCounter.totalTransferred = transferCounter.totalTransferred + event.params.value
+  transferCounter.save()
+  transferCounter.id = day.toString()
+  transferCounter.save()
 }
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
-  }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+function newUser(id: string, address: string): User {
+  let user = new User(id);
+  user.address = address
+  user.balance = BigInt.fromI32(0)
+  user.transactionCount = 0
+  return user
 }
